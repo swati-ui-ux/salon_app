25d3a3c3189cd5134ce1479435f6ecdcf1a0876a
Swati singh <swatigola274@email.com>
2026-05-14 13:02:43 +0530

    add port

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 require("dotenv").config()
 
 const app = express()
-const PORT = process.env.PORT||4000
+const PORT = process.env.PORT||5000
 
 const sequelize = require("./config/db")
 
