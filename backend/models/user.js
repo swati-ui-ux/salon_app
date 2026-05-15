10b66afb46be0264573dd89b8aa33ea415903003
Swati singh <swatigola274@email.com>
2026-05-15 16:46:57 +0530

    correct skills in user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,90 +2,42 @@
 
 const { DataTypes } = require("sequelize")
 const sequelize = require("../config/db")
-
 const User = sequelize.define("User", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
 
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-        }
-    },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 
-    phone: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+  skills: {
+    type: DataTypes.TEXT,
+    defaultValue: "[]",
+  },
 
-    careerGoal: {
-        type: DataTypes.TEXT,
-        defaultValue: ""
-    },
-
-    profileImage: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    linkedinUrl: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    githubUrl: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    portfolioUrl: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    skills: {
-        type: DataTypes.JSON,
-        defaultValue: []
-    },
-
-    experienceLevel: {
-        type: DataTypes.ENUM(
-            "Fresher",
-            "Junior",
-            "Mid-Level",
-            "Senior"
-        ),
-        defaultValue: "Fresher"
-    },
-
-    city: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    country: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-
-    isAdmin: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
-
-}, {
-    timestamps: true
-})
+  experienceLevel: {
+    type: DataTypes.ENUM(
+      "Fresher",
+      "Junior",
+      "Mid-Level",
+      "Senior"
+    ),
+    defaultValue: "Fresher",
+  },
+});
 
 module.exports = User
